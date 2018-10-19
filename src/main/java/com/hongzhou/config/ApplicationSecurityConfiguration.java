f4b2aaa8c2e17f381d9e7e2e1ddfe1b10ea591f5
Hong Zhou <zhsnnu@gmail.com>
2018-10-18 22:57:38 -0600

    implemented basic in-memory authentication

---
+++ src/main/java/com/hongzhou/config/ApplicationSecurityConfiguration.java
@@ -0,0 +1,40 @@
+package com.hongzhou.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+			.authorizeRequests()
+			.antMatchers("/", "/index", "/css/*", "/js/*").permitAll()
+			.anyRequest().authenticated()
+			.and()
+			.httpBasic();
+	}
+
+	@Bean
+	@Override
+	protected UserDetailsService userDetailsService() {
+		List<UserDetails> users = new ArrayList<>();
+		users.add(User.withDefaultPasswordEncoder().username("hong").password("password").roles("USER", "ADMIN").build());
+		users.add(User.withDefaultPasswordEncoder().username("grace").password("grace").roles("USER").build());
+		return new InMemoryUserDetailsManager(users);
+	}
+
+	
+}
