b53fb362f9c3d00b62338dd295e22bacff15207b
Hong Zhou <zhsnnu@gmail.com>
2018-10-20 11:26:52 -0600

    implement LDAP authentication with embedded LDAP server

--- src/main/java/com/hongzhou/config/ApplicationSecurityConfiguration.java
+++ src/main/java/com/hongzhou/config/ApplicationSecurityConfiguration.java
@@ -1,9 +1,7 @@
 package com.hongzhou.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,28 +9,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import com.hongzhou.auth.HotelUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter {
-	
-	@Autowired
-	private HotelUserDetailsService userDetailsService;
-	
-	@Bean
-	public DaoAuthenticationProvider authenticationProvider() {
-		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-		provider.setUserDetailsService(userDetailsService);
-		//provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
-		provider.setPasswordEncoder(new BCryptPasswordEncoder(11));
-		provider.setAuthoritiesMapper(authoritiesMapper());
-		return provider;
-	}
 	
 	@Bean
 	public GrantedAuthoritiesMapper authoritiesMapper() {
@@ -44,7 +27,16 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.authenticationProvider(authenticationProvider());
+		auth.ldapAuthentication()
+			.userDnPatterns("uid={0},ou=people")
+			.groupSearchBase("ou=groups")
+			.authoritiesMapper(authoritiesMapper())
+			.contextSource()
+			.url("ldap://localhost:8389/dc=frankmoley,dc=com")
+			.and()
+			.passwordCompare()
+			.passwordEncoder(new LdapShaPasswordEncoder())
+			.passwordAttribute("userPassword");
 	}
 
 	@Override
@@ -63,19 +55,5 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
 			.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
 			.logoutSuccessUrl("/logout-success").permitAll();
 	}
-
-	/*
-	 * In-memory basic authentication
-	 * 
-	
-	@Bean
-	@Override
-	protected UserDetailsService userDetailsService() {
-		List<UserDetails> users = new ArrayList<>();
-		users.add(User.withDefaultPasswordEncoder().username("hong").password("password").roles("USER", "ADMIN").build());
-		users.add(User.withDefaultPasswordEncoder().username("grace").password("grace").roles("USER").build());
-		return new InMemoryUserDetailsManager(users);
-	}
-   */
 	
 }
